7824eb35a98bcf044fc61167c20663c22888d1e0
CyrikJun <johnk2001@naver.com>
2024-12-24 14:39:24 +0900

    Title: 날짜 변경하여 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241224/날짜 변경하여 출력/print-date-with-different-format.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int y, m, d;
+    char b;
+    char t = '-';
+
+    cin >> y >> b >> m >> b >> d;
+    cout << m << t << d << t << y << endl;
+
+    return 0;
+}
