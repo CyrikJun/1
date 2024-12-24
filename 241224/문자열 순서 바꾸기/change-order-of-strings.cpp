293121dafa169583c980fccfabeb4ce57f287ed8
CyrikJun <johnk2001@naver.com>
2024-12-24 14:27:10 +0900

    Title: 문자열 순서 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241224/문자열 순서 바꾸기/change-order-of-strings.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    string t;
+
+    cin >> s >> t;
+
+    cout << t << endl << s << endl;
+
+
+    return 0;
+}
