b1efbd5c92208ef85eab639a29aa3088515984ec
CyrikJun <johnk2001@naver.com>
2024-12-24 15:28:40 +0900

    Title: 살 수 있는 물건 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241224/살 수 있는 물건 2/things-able-to-buy-2.cpp
+++ 241224/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -7,11 +7,11 @@ int main() {
 
     const int book = 3000, mask = 1000, pen = 500;
 
-    if (money >= 3000)
+    if (money >= book)
         cout << "book" << endl;
-    else if (money < 3000 && money >= 1000)
-        cout << "mast" << endl;
-    else if (money < 1000 && money >= 500)
+    else if (money < book && money >= mask)
+        cout << "mask" << endl;
+    else if (money < mask && money >= pen)
         cout << "pen" << endl;
     else
         cout << "no" << endl;
